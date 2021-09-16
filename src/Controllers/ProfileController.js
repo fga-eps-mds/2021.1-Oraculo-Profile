7c32b7655f348acf2584ebcd4f11e9d976b9ac42
jpaulohe4rt <jpaulo.on@gmail.com>
2021-09-16 20:35:47 -0300

    :sparkles: add ProfilesController in path Controllers #33

---
+++ src/Controllers/ProfileController.js
@@ -0,0 +1,24 @@
+const db = require("../models");
+const Users = db.users;
+
+exports.create = (req, res) => {
+  if (!req.body.email && !req.body.password) {
+    res.status(400).send({
+      message: "error users could , all fields are required!",
+    });
+    return res.status(200).sen({ message: "profile created with success" });
+  }
+
+  const creatingUsers = {
+    email: req.body.email,
+    password: req.body.password,
+  };
+
+  Users.create(creatingUsers)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      return res.status(500).json({ message: "error creating an user" });
+    });
+};
