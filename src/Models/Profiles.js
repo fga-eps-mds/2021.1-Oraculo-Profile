d8386c13842e0895db84e89f91dec81063592026
jpaulohe4rt <jpaulo.on@gmail.com>
2021-09-16 20:24:24 -0300

    :sparkles: add Schema Profiles in path Models #33

---
+++ src/Models/Profiles.js
@@ -0,0 +1,21 @@
+const { Model, Sequelize } = require("sequelize");
+
+class Profiles extends Model {
+  static init(db) {
+    super.init(
+      {
+        email: {
+          type: Sequelize.STRING,
+        },
+        password: {
+          type: Sequelize.STRING,
+        },
+      },
+      {
+        sequelize: db,
+      }
+    );
+  }
+}
+
+module.exports = Profiles;
