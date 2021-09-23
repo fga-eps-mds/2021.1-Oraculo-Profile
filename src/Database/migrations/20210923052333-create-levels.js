9d67a056b0175c1d08adeb1ce84f2ee6e71544bd
Lucas Vieira <lucas.engen@outlook.com>
2021-09-23 03:03:40 -0300

    fga-eps-mds/2021.1-Oraculo-Profile#33 :tada: adds table to store access levels information
    Co-authored-by: Gabriel Santos Silva Araújos <gabrielsantos_s96@hotmail.com>

---
+++ src/Database/migrations/20210923052333-create-levels.js
@@ -0,0 +1,44 @@
+"use strict";
+
+module.exports = {
+	up: async (queryInterface, Sequelize) => {
+		/**
+		 * Add altering commands here.
+		 *
+		 * Example:
+		 * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+		 */
+
+		return queryInterface.createTable("levels", {
+			id: {
+				type: Sequelize.INTEGER,
+				primaryKey: true,
+				autoIncrement: true,
+				allowNull: false,
+			},
+			name: {
+				type: Sequelize.TEXT,
+				allowNull: false,
+			},
+			created_at: {
+				type: Sequelize.DATE,
+				allowNull: false,
+			},
+			updated_at: {
+				type: Sequelize.DATE,
+				allowNull: false,
+			},
+		});
+	},
+
+	down: async (queryInterface, Sequelize) => {
+		/**
+		 * Add reverting commands here.
+		 *
+		 * Example:
+		 * await queryInterface.dropTable('users');
+		 */
+
+		return queryInterface.dropTable("levels");
+	},
+};
