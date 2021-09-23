32216a9f4cc7037de2bc27382c3a166a53766237
Lucas Vieira <lucas.engen@outlook.com>
2021-09-23 02:52:25 -0300

    fga-eps-mds/2021.1-Oraculo-Profile#33 :recycle: removes useless fields from model - :sparkles: adds relationship to 'User' model
    Co-authored-by: Gabriel Santos Silva Araújos <gabrielsantos_s96@hotmail.com>

--- src/Models/User.js
+++ src/Models/User.js
@@ -1,39 +1,27 @@
 const { Model, Sequelize } = require("sequelize");
 
-class Users extends Model {
-  static init(db) {
-    super.init(
-      {
-        id: {
-          type: Sequelize.INTEGER,
-          primaryKey: true,
-          autoIncrement: true,
-          allowNull: false,
-        },
-        permission: {
-          type: Sequelize.INTEGER,
-        },
-        password: {
-          type: Sequelize.TEXT,
-        },
-        email: {
-          type: Sequelize.TEXT,
-        },
-        departmentID: {
-          type: Sequelize.INTEGER,
-        },
-        level: {
-          type: Sequelize.INTEGER,
-        },
-        sectionID: {
-          type: Sequelize.INTEGER,
-        },
-      },
-      {
-        sequelize: db,
-      }
-    );
-  }
+class User extends Model {
+	static init(db) {
+		super.init(
+			{
+				id: { type: Sequelize.INTEGER, primaryKey: true },
+				permission: { type: Sequelize.INTEGER },
+				password: { type: Sequelize.TEXT },
+				email: { type: Sequelize.TEXT },
+				departmentID: { type: Sequelize.INTEGER },
+				level: { type: Sequelize.INTEGER },
+				sectionID: { type: Sequelize.INTEGER },
+			},
+			{
+				sequelize: db,
+			}
+		);
+	}
+
+	static associate(models) {
+		User.hasOne(models.Department);
+		User.hasOne(models.userLevel);
+	}
 }
 
-module.exports = Users;
+module.exports = User;
