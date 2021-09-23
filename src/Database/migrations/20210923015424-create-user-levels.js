fd29ead8eabd4f730b90a3c149b8ed2f9057c9b7
Lucas Vieira <lucas.engen@outlook.com>
2021-09-23 03:02:12 -0300

    fga-eps-mds/2021.1-Oraculo-Profile#33 :tada: adds table for user levels
    Co-authored-by: Gabriel Santos Silva Araújos <gabrielsantos_s96@hotmail.com>

---
+++ src/Database/migrations/20210923015424-create-user-levels.js
@@ -0,0 +1,46 @@
+"use strict";
+
+module.exports = {
+	up: async (queryInterface, Sequelize) => {
+		/**
+		 * Add altering commands here.
+		 *
+		 * Example:
+		 * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+		 */
+
+		return queryInterface.createTable("user_levels", {
+			id: {
+				type: Sequelize.INTEGER,
+				primaryKey: true,
+				autoIncrement: true,
+				allowNull: false,
+			},
+			user_id: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				references: { model: "users", key: "id" },
+				onUpdate: "CASCADE",
+				onDelete: "CASCADE",
+			},
+			level_id: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				references: { model: "levels", key: "id" },
+				onUpdate: "CASCADE",
+				onDelete: "CASCADE",
+			},
+		});
+	},
+
+	down: async (queryInterface, Sequelize) => {
+		/**
+		 * Add reverting commands here.
+		 *
+		 * Example:
+		 * await queryInterface.dropTable('users');
+		 */
+
+		return queryInterface.dropTable("user_levels");
+	},
+};
