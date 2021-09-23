be0bef48a3248513781e7271cd9d931004d6cf1e
Lucas Vieira <lucas.engen@outlook.com>
2021-09-23 02:43:05 -0300

    fga-eps-mds/2021.1-Oraculo#33 :recycle: creates a function to perform database client initialization - Adds functions to perform models initialization - Adds logging into screen after HTTP server initialization
    Co-authored-by: Gabriel Santos Silva Araújos <gabrielsantos_s96@hotmail.com>

---
+++ src/Database/index.js
@@ -0,0 +1,35 @@
+const User = require("../Models/User");
+const config = require("./config/database");
+const { Sequelize } = require("sequelize");
+const Department = require("../Models/Department");
+const userLevel = require("../Models/userLevel");
+
+async function initializeDatabase() {
+	const db = new Sequelize(config);
+
+	let auth = db.authenticate();
+
+	return new Promise((resolve, reject) => {
+		auth.then(
+			() => {
+				User.init(db);
+				Department.init(db);
+				userLevel.init(db);
+
+				User.associate(db.models);
+				Department.associate(db.models);
+				userLevel.associate(db.models);
+
+				resolve(0);
+			},
+			(rejected) => {
+				console.error(`failed to authenticate: ${rejected}`);
+				reject(1);
+			}
+		);
+	});
+}
+
+module.exports = {
+	initializeDatabase,
+};
--- src/index.js
+++ src/index.js
@@ -1,41 +1,32 @@
 const express = require("express");
 const cors = require("cors");
 const routes = require("./routes");
-const { Sequelize } = require("sequelize");
 const env = require("dotenv");
-const User = require("./Models/User");
-const Users = require("./Models/User");
+const { initializeDatabase } = require("./Database");
 
 env.config();
 
-const { DB_USER, DB_PASS, DB_HOSTNAME, DB_NAME, DB_PORT, APP_PORT } =
-  process.env;
+const { APP_PORT } = process.env;
 
 const app = express();
 app.disable("x-powered-by");
 
-const db = new Sequelize({
-  username: `${DB_USER}`,
-  password: `${DB_PASS}`,
-  database: `${DB_NAME}`,
-  port: `${DB_PORT}`,
-  dialect: "postgres",
-  host: Number.parseInt(`${DB_HOSTNAME}`),
-  define: {
-    timestamps: true,
-    underscored: true,
-  },
-});
-
-db.authenticate().catch((reason) => {
-  console.error(`failed to authenticate: ${reason}`);
-});
-
-Users.init(db);
-
-app.listen(APP_PORT);
 app.use(cors({ origin: "localhost:8000" }));
 app.use(express.json());
 app.use(routes);
+
+if (require.main == module) {
+	app.listen(APP_PORT);
+	console.info(`Serving HTTP at: http://localhost:${APP_PORT}`);
+
+	initializeDatabase().then(
+		() => {
+			console.info(`connected to database`);
+		},
+		() => {
+			console.error(`could not connect to database`);
+		}
+	);
+}
 
 module.exports = app;
