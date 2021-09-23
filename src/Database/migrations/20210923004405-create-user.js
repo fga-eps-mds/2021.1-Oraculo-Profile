47fe8853889409f5e58386023e4abba5f5ba4cdb
Lucas Vieira <lucas.engen@outlook.com>
2021-09-23 02:59:25 -0300

    fga-eps-mds/2021.1-Oraculo-Profile#33 :recycle: fixes field names
    Co-authored-by: Gabriel Santos Silva Araújos <gabrielsantos_s96@hotmail.com>

--- src/Database/migrations/20210923004405-create-user.js
+++ src/Database/migrations/20210923004405-create-user.js
@@ -1,42 +1,50 @@
 "use strict";
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    return queryInterface.createTable("users", {
-      id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false,
-      },
-      permission: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-      },
-      password: {
-        type: Sequelize.TEXT,
-        allowNull: false,
-      },
-      email: {
-        type: Sequelize.TEXT,
-        allowNull: false,
-      },
-      departmentID: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-      },
-      level: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-      },
-      sectionID: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-      },
-    });
-  },
+	up: async (queryInterface, Sequelize) => {
+		return queryInterface.createTable("users", {
+			id: {
+				type: Sequelize.INTEGER,
+				primaryKey: true,
+				autoIncrement: true,
+				allowNull: false,
+			},
+			permission: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+			},
+			password: {
+				type: Sequelize.TEXT,
+				allowNull: false,
+			},
+			email: {
+				type: Sequelize.TEXT,
+				allowNull: false,
+			},
+			department_id: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+			},
+			level: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+			},
+			section_id: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+			},
+			created_at: {
+				type: Sequelize.DATE,
+				allowNull: false,
+			},
+			updated_at: {
+				type: Sequelize.DATE,
+				allowNull: false,
+			},
+		});
+	},
 
-  down: async (queryInterface, Sequelize) => {
-    return queryInterface.dropTable("users");
-  },
+	down: async (queryInterface, Sequelize) => {
+		return queryInterface.dropTable("users");
+	},
 };
