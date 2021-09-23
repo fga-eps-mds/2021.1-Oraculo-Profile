123cebc78eef7b97cf8d790e120e1ea57eb3f61b
jpaulohe4rt <jpaulo.on@gmail.com>
2021-09-22 22:35:39 -0300

    :art: improving 'src/index.js' fga-eps-mds/2021.1-Oraculo-Profile#33
    Co-authored-by: Gabriel Santos Silva Araújos <gabrielsantos_s96@hotmail.com>
    Co-authored-by: Antonio Igor Carvalho <antonioigorcarvalho@gmail.com>
    Co-authored-by: Lucas Vieira de Jesus <lucas.engen.cc@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ const cors = require("cors");
 const routes = require("./routes");
 const { Sequelize } = require("sequelize");
 const env = require("dotenv");
-const Profile = require("./Model/Profile");
+const User = require("./Models/User");
+const Users = require("./Models/User");
 
 env.config();
 
-const { DB_USER, DB_PASS, DB_HOSTNAME, APP_PORT } = process.env;
+const { DB_USER, DB_PASS, DB_HOSTNAME, DB_NAME, DB_PORT, APP_PORT } =
+  process.env;
 
 const app = express();
 app.disable("x-powered-by");
@@ -15,6 +17,8 @@ app.disable("x-powered-by");
 const db = new Sequelize({
   username: `${DB_USER}`,
   password: `${DB_PASS}`,
+  database: `${DB_NAME}`,
+  port: `${DB_PORT}`,
   dialect: "postgres",
   host: Number.parseInt(`${DB_HOSTNAME}`),
   define: {
@@ -27,10 +31,10 @@ db.authenticate().catch((reason) => {
   console.error(`failed to authenticate: ${reason}`);
 });
 
-Profile.init(db);
+Users.init(db);
 
 app.listen(APP_PORT);
-app.use(cors({origin: 'localhost:8000'}));
+app.use(cors({ origin: "localhost:8000" }));
 app.use(express.json());
 app.use(routes);
 
