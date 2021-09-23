dc8758edcf0d4bd256b426178a2b1ce44a81881e
Lucas Vieira <lucas.engen@outlook.com>
2021-09-23 02:50:08 -0300

    fga-eps-mds/2021.1-Oraculo-Profile#33 :sparkles: adds relationship to User model - :recycle: removes useless fields in model
    Co-authored-by: Gabriel Santos Silva Araújos <gabrielsantos_s96@hotmail.com>

--- src/Models/Department.js
+++ src/Models/Department.js
@@ -1,24 +1,21 @@
 const { Model, Sequelize } = require("sequelize");
 
 class Department extends Model {
-  static init(db) {
-    super.init(
-      {
-        id: {
-          type: Sequelize.INTEGER,
-          primaryKey: true,
-          autoIncrement: true,
-          allowNull: false,
-        },
-        name: {
-          type: Sequelize.TEXT,
-        },
-      },
-      {
-        sequelize: db,
-      }
-    );
-  }
+	static init(db) {
+		super.init(
+			{
+				id: { type: Sequelize.INTEGER, primaryKey: true },
+				name: { type: Sequelize.TEXT },
+			},
+			{
+				sequelize: db,
+			}
+		);
+	}
+
+	static associate(models) {
+		Department.hasOne(models.User);
+	}
 }
 
 module.exports = Department;
