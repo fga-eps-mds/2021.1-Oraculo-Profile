d7ac26a9b3243295f9d61aba2ebe267bc9b41fc1
Lucas Vieira <lucas.engen@outlook.com>
2021-09-25 15:52:22 -0300

    fga-eps-mds/2021.1-Oraculo#33 :sparkles: implements model inicialization and association for 'section' model
    Co-authored-by: Gabriel Santos Silva Araújos <gabrielsantos_s96@hotmail.com>

--- src/Database/index.js
+++ src/Database/index.js
@@ -3,6 +3,7 @@ const config = require("./config/database");
 const { Sequelize } = require("sequelize");
 const { Department } = require("../Model/Department");
 const { Level } = require("../Model/Level");
+const { Section } = require("../Model/Section");
 
 async function initializeDatabase() {
 	const db = new Sequelize(config);
@@ -15,10 +16,12 @@ async function initializeDatabase() {
 				User.init(db);
 				Department.init(db);
 				Level.init(db);
+				Section.init(db);
 
 				User.associate(db.models);
 				Department.associate(db.models);
 				Level.associate(db.models);
+				Section.associate(db.models);
 
 				resolve(0);
 			},
