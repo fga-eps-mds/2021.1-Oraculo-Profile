6009d19a31787ce70989a9dc8dd06e8cce5f6c29
Lucas Vieira <lucas.engen@outlook.com>
2021-09-26 03:25:59 -0300

    fga-eps-mds/2021.1-Oraculo#86 :white_check_mark: adds more tests
    Co-authored-by: Gabriel Santos Silva Araújos <gabrielsantos_s96@hotmail.com>

--- tests/index.test.js
+++ tests/index.test.js
@@ -5,33 +5,56 @@ const { initializeDatabase } = require("../src/Database");
 
 describe("Main test", () => {
 	const user = {
-		email: "test1@gmail.com",
-		password: "password123",
-		departmentID: 2,
-		level: 0,
-		sectionID: 1,
+		email: "useroraculo@email.com",
+		password: "oraculo123",
+		departmentID: 3,
+		level: 2,
+		sectionID: 3,
 	};
 
 	beforeAll(async () => {
-		await request(app).post("/register").send({ user });
 		await initializeDatabase();
+		await request(app).post("/register").send(user);
+		return;
 	});
 
 	it("Test express server app", (done) => {
 		expect(app).toBeDefined();
 		done();
-	});
-
-	it("Test register route", async () => {
-		const res = await request(app).post("/register").send(user);
-		expect(res.statusCode).toEqual(401);
 		return;
 	});
 
-	it("Test login route", async () => {
+	it("POST /register - should not create because user already exists", async () => {
+		const res = await request(app).post("/register").send(user);
+		expect(res.statusCode).toEqual(400);
+		return res;
+	});
+
+	it("POST /login - should login", async () => {
+		const res = await request(app)
+			.post("/login")
+			.send({ email: user.email, password: user.password });
+
+		expect(res.statusCode).toBe(200);
+		return res;
+	});
+
+	it("POST /login - should not login", async () => {
 		const res = await request(app)
 			.post("/login")
 			.send({ email: "user@gmail.com", password: "12345" });
+
+		expect(res.statusCode).toBe(401);
+		return res;
+	});
+
+	it("POST /login - should not login (invalid credentials)", async () => {
+		const res = await request(app)
+			.post("/login")
+			.send({ email: user.email, password: "wrongpassword" });
+
+		expect(res.statusCode).toBe(401);
+		return res;
 	});
 });
 
