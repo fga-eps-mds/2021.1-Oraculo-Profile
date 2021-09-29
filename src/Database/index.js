956732ee30e0f0d838e7c54083eb426bfc033b92
Lucas Vieira <lucas.engen@outlook.com>
2021-09-29 00:31:43 -0300

    fga-eps-mds/2021.1-Oraculo#40 refactor: :recycle: abstracts functions to setup database client
    Co-authored-by: Gabriel Santos Silva Araújos <gabrielsantos_s96@hotmail.com>

--- src/Database/index.js
+++ src/Database/index.js
@@ -5,34 +5,37 @@ const { Department } = require("../Model/Department");
 const { Level } = require("../Model/Level");
 const { Section } = require("../Model/Section");
 
+async function setupModels(db) {
+    User.init(db);
+    Department.init(db);
+    Level.init(db);
+    Section.init(db);
+
+    User.associate(db.models);
+    Department.associate(db.models);
+    Level.associate(db.models);
+    Section.associate(db.models);
+}
+
+async function setupSequelize(config) {
+    return new Sequelize(config);
+}
+
+async function configure(auth, db) {
+    return new Promise((resolve, reject) => {
+        auth.then(() => {
+            setupModels(db);
+            resolve(0);
+        });
+    });
+}
+
 async function initializeDatabase() {
-	const db = new Sequelize(config);
-
-	const auth = db.authenticate();
-
-	return new Promise((resolve, reject) => {
-		auth.then(
-			() => {
-				User.init(db);
-				Department.init(db);
-				Level.init(db);
-				Section.init(db);
-
-				User.associate(db.models);
-				Department.associate(db.models);
-				Level.associate(db.models);
-				Section.associate(db.models);
-
-				resolve(0);
-			},
-			(rejected) => {
-				console.error(`failed to authenticate: ${rejected}`);
-				reject(1);
-			}
-		);
-	});
+    const db = await setupSequelize(config);
+    const auth = db.authenticate();
+    return configure(auth, db);
 }
 
 module.exports = {
-	initializeDatabase,
+    initializeDatabase,
 };
