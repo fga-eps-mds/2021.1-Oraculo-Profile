cd1d4f2069a2f49c713d4c05924c527bed60a054
Lucas Vieira <lucas.engen@outlook.com>
2021-10-14 19:47:17 -0300

    chore: extracts database login info from a single environment variable

--- src/Database/index.js
+++ src/Database/index.js
@@ -6,36 +6,38 @@ const { Level } = require("../Model/Level");
 const { Section } = require("../Model/Section");
 
 async function setupModels(db) {
-    User.init(db);
-    Department.init(db);
-    Level.init(db);
-    Section.init(db);
+  User.init(db);
+  Department.init(db);
+  Level.init(db);
+  Section.init(db);
 
-    User.associate(db.models);
-    Department.associate(db.models);
-    Level.associate(db.models);
-    Section.associate(db.models);
+  User.associate(db.models);
+  Department.associate(db.models);
+  Level.associate(db.models);
+  Section.associate(db.models);
 }
 
 async function setupSequelize(config) {
-    return new Sequelize(config);
+  require("dotenv").config();
+  console.info(`connecting to ${process.env.DATABASE_URL}`);
+  return new Sequelize(process.env.DATABASE_URL);
 }
 
 async function configure(auth, db) {
-    return new Promise((resolve, reject) => {
-        auth.then(() => {
-            setupModels(db);
-            resolve(0);
-        });
+  return new Promise((resolve, reject) => {
+    auth.then(() => {
+      setupModels(db);
+      resolve(0);
     });
+  });
 }
 
 async function initializeDatabase() {
-    const db = await setupSequelize(config);
-    const auth = db.authenticate();
-    return configure(auth, db);
+  const db = await setupSequelize(config);
+  const auth = db.authenticate();
+  return configure(auth, db);
 }
 
 module.exports = {
-    initializeDatabase,
+  initializeDatabase,
 };
