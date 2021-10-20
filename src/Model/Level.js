5475edb6ae918cf8861bc9514086446769243ef4
Lucas Vieira <lucas.engen@outlook.com>
2021-10-20 01:06:03 -0300

    fga-eps-mds/2021.1-Oraculo#98 refactor: fixes file indent
    Co-authored-by: Antonio Igor Carvalho <antonioigorcarvalho@gmail.com>

--- src/Model/Level.js
+++ src/Model/Level.js
@@ -1,25 +1,25 @@
 const { Model, Sequelize } = require("sequelize");
 
 class Level extends Model {
-	static init(db) {
-		super.init(
-			{
-				name: { type: Sequelize.TEXT },
-			},
-			{
-				sequelize: db,
-				tableName: "levels",
-			}
-		);
-	}
+  static init(db) {
+    super.init(
+      {
+        name: { type: Sequelize.TEXT },
+      },
+      {
+        sequelize: db,
+        tableName: "levels",
+      }
+    );
+  }
 
-	static associate(models) {
-		this.belongsToMany(models.User, {
-			foreignKey: "level_id",
-			through: "user_levels",
-			as: "levels",
-		});
-	}
+  static associate(models) {
+    this.belongsToMany(models.User, {
+      foreignKey: "level_id",
+      through: "user_levels",
+      as: "levels",
+    });
+  }
 }
 
 module.exports = { Level };
