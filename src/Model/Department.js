d1e59da54e2185df267941289c527511f46b2c12
Lucas Vieira <lucas.engen@outlook.com>
2021-10-20 01:54:50 -0300

    fga-eps-mds/2021.1-Oraculo#98 feat: adds method to return empty value for department or section
    Co-authored-by: Antonio Igor Carvalho <antonioigorcarvalho@gmail.com>

--- src/Model/Department.js
+++ src/Model/Department.js
@@ -1,25 +1,39 @@
 const { Model, Sequelize } = require("sequelize");
 
 class Department extends Model {
-	static init(db) {
-		super.init(
-			{
-				name: { type: Sequelize.TEXT },
-			},
-			{
-				sequelize: db,
-				tableName: "departments",
-			}
-		);
-	}
+  static init(db) {
+    super.init(
+      {
+        name: { type: Sequelize.TEXT },
+      },
+      {
+        sequelize: db,
+        tableName: "departments",
+      }
+    );
+  }
 
-	static associate(models) {
-		this.belongsToMany(models.User, {
-			foreignKey: "department_id",
-			through: "user_departments",
-			as: "departments",
-		});
-	}
+  static associate(models) {
+    this.belongsToMany(models.User, {
+      foreignKey: "department_id",
+      through: "user_departments",
+      as: "departments",
+    });
+  }
+
+  static getEmpty() {
+    try {
+      return await Department.findOne({
+        attributes: ["id", "name"],
+        where: {
+          name: "none",
+        },
+      });
+    } catch (err) {
+      console.error(`internal error ==> could not get departments: ${err}`);
+      return null;
+    }
+  }
 }
 
 module.exports = { Department };
--- src/Model/Section.js
+++ src/Model/Section.js
@@ -1,25 +1,39 @@
 const { Model, Sequelize } = require("sequelize");
 
 class Section extends Model {
-	static init(db) {
-		super.init(
-			{
-				name: { type: Sequelize.TEXT },
-			},
-			{
-				sequelize: db,
-				tableName: "sections",
-			}
-		);
-	}
+  static init(db) {
+    super.init(
+      {
+        name: { type: Sequelize.TEXT },
+      },
+      {
+        sequelize: db,
+        tableName: "sections",
+      }
+    );
+  }
 
-	static associate(models) {
-		this.belongsToMany(models.User, {
-			foreignKey: "section_id",
-			through: "user_sections",
-			as: "sections",
-		});
-	}
+  static associate(models) {
+    this.belongsToMany(models.User, {
+      foreignKey: "section_id",
+      through: "user_sections",
+      as: "sections",
+    });
+  }
+
+  static getEmpty() {
+    try {
+      return await Section.findOne({
+        attributes: ["id", "name"],
+        where: {
+          name: "none",
+        },
+      });
+    } catch (err) {
+      console.error(`internal error => could not get sections: ${err}`);
+      return null;
+    }
+  }
 }
 
 module.exports = { Section };
