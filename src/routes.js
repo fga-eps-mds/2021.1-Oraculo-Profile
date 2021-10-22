87252471c3dfc09922e784b1a495b95beb1320f7
jpaulohe4rt <jpaulo.on@gmail.com>
2021-10-22 01:43:03 -0300

    :sparkles: add router for updatePassword fga-eps-mds/2021.1-Oraculo-Profile#51
    Co-authored-by: Gabriel Santos Silva Araújos <gabrielsantos_s96@hotmail.com>

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.post("/register", jwt.verifyJWT, UserController.createUser);
 router.post("/login", UserController.loginUser);
-router.post("/user/reset", jwt.verifyJWT, UserController.updatePassword);
+router.post("/user/change-password", jwt.verifyJWT, UserController.updatePassword)
 router.get("/users/all", jwt.verifyJWT, UserController.getUsersList);
 router.get("/user/access-level", jwt.verifyJWT, UserController.getAccessLevel);
 router.get("/user/info", jwt.verifyJWT, UserController.getUserInfo);
