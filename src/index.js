c0e41ad6efd50c40ce7f3b5772cc057c0733d482
Lucas Vieira <lucas.engen@outlook.com>
2021-12-03 02:26:26 -0300

    chore: disables cors

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const corsOptions = {
 const app = express();
 app.disable("x-powered-by");
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(morgan("short"));
 app.use(routes);
